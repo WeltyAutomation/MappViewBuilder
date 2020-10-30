04fab584111ec8d9a7b697987dc241118f85204a
Tyler Woodley <tyler@weltyautomation.com>
2020-10-29 22:57:05 -0500

    Refactoring

--- MappViewBuilder/ParseFbkTests.cs
+++ MappViewBuilder/ParseFbkTests.cs
@@ -27,7 +27,7 @@ namespace MappViewBuilder
                 END_FUNCTION_BLOCK";
 
 
-            var fbkName = GetFbkName(funContent);
+            var fbkName = Fbk.GetFbkName(funContent);
 
             fbkName.Should().Be("SampleFBK");
         }
@@ -48,7 +48,7 @@ namespace MappViewBuilder
                     END_VAR
                 END_FUNCTION_BLOCK";
 
-            var flatContent = FlattenDefinitionText(funContent);
+            var flatContent = Fbk.FlattenDefinitionText(funContent);
             flatContent.Should()
                 .Be("FUNCTION_BLOCK SampleFBK,VAR_INPUT,InputVariable : BOOL;,END_VAR,VAR_OUTPUT,OutputVariable: { REDUND_UNREPLICABLE} BOOL;,END_VAR,VAR,InternalVariable: TransferConveyorStateEnum;,END_VAR,END_FUNCTION_BLOCK");
         }
@@ -69,7 +69,7 @@ namespace MappViewBuilder
                     END_VAR
                 END_FUNCTION_BLOCK";
 
-            var variableGroupTexts = GetVariableGroupTexts(funContent);
+            var variableGroupTexts = Fbk.GetVariableGroupTexts(funContent);
 
 
             variableGroupTexts.Length.Should().Be(3);
@@ -85,9 +85,9 @@ namespace MappViewBuilder
             var outputText = "VAR_OUTPUT,OutputVariable : { REDUND_UNREPLICABLE} BOOL;";
             var localText = "VAR,InternalVariable : TransferConveyorStateEnum;";
 
-            GetVariableCategory(inputText).Should().Be("Input");
-            GetVariableCategory(outputText).Should().Be("Output");
-            GetVariableCategory(localText).Should().Be("Local");
+            FbkVar.GetVariableCategory(inputText).Should().Be(FbkVarCategory.Input);
+            FbkVar.GetVariableCategory(outputText).Should().Be(FbkVarCategory.Output);
+            FbkVar.GetVariableCategory(localText).Should().Be(FbkVarCategory.Local);
         }
 
         [Fact]
@@ -95,7 +95,7 @@ namespace MappViewBuilder
         {
             var outputText = "VAR_OUTPUT,var0 : { REDUND_UNREPLICABLE} BOOL;,var1 : TestableTON;,var2 : TT_Mark_DKFV_InputConfigFBK;";
 
-            var variableDefinitions = GetVariableDefinitions(outputText);
+            var variableDefinitions = FbkVar.GetVariableDefinitions(outputText);
 
             variableDefinitions[0].Should().Be("var0 : { REDUND_UNREPLICABLE} BOOL;");
             variableDefinitions[1].Should().Be("var1 : TestableTON;");
@@ -109,9 +109,9 @@ namespace MappViewBuilder
             var var1 = "var1 : TestableTON;";
             var var2 = "var2 : TT_Mark_DKFV_InputConfigFBK;";
 
-            var var0Tuple = ParseVariable(var0);
-            var var1Tuple = ParseVariable(var1);
-            var var2Tuple = ParseVariable(var2);
+            var var0Tuple = FbkVar.ParseVariable(var0);
+            var var1Tuple = FbkVar.ParseVariable(var1);
+            var var2Tuple = FbkVar.ParseVariable(var2);
 
             var0Tuple.name.Should().Be("var0");
             var0Tuple.type.Should().Be("BOOL");
@@ -123,45 +123,118 @@ namespace MappViewBuilder
             var2Tuple.type.Should().Be("TT_Mark_DKFV_InputConfigFBK");
         }
 
-        private string GetFbkName(string funContent)
+        [Fact]
+        public void WeCanParseFbkTextToObject()
+        {
+            var funContent =
+                @"FUNCTION_BLOCK SampleFBK
+                    VAR_INPUT
+                        InputVariable : BOOL;
+                    END_VAR
+                    VAR_OUTPUT
+                        OutputVariable : { REDUND_UNREPLICABLE} BOOL;
+                    END_VAR
+                    VAR
+                        InternalVariable : TransferConveyorStateEnum;
+                    END_VAR
+                END_FUNCTION_BLOCK";
+
+            var fbk = new Fbk(funContent);
+
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    public class Fbk
+    {
+        public Fbk(string content)
+        {
+            Name = GetFbkName(content);
+
+        }
+
+        public string Name;
+        public List<FbkVar> Variables;
+
+        public static string GetFbkName(string funContent)
         {
             var match = Regex.Match(funContent, @"FUNCTION_BLOCK (\w+)");
             return match.Success ? match.Groups[1].Value : string.Empty;
         }
 
-        private string FlattenDefinitionText(string content)
+        //public static List<FbkVar> GetVariables(string funContent)
+        //{
+
+
+        //}
+
+        public static string FlattenDefinitionText(string content)
         {
             return string.Join(",", content.Split(Environment.NewLine).Select(s => s.Trim()));
         }
 
-        private string[] GetVariableGroupTexts(string content)
+        public static string[] GetVariableGroupTexts(string content)
         {
-            var flatContent = FlattenDefinitionText(content);
+            var flatContent = Fbk.FlattenDefinitionText(content);
             var variableGroupRegex = new Regex(@"VAR.*END_VAR", RegexOptions.None);
             var match = variableGroupRegex.Match(flatContent);
             return match.Value.Split("END_VAR", StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim(',')).ToArray();
         }
+    }
 
-        private string GetVariableCategory(string variableGroupText)
+    public class FbkVar
+    {
+        public FbkVar(string variableText, FbkVarCategory category)
         {
-            var header = variableGroupText.Split(',').FirstOrDefault();
-
-            return string.IsNullOrWhiteSpace(header) ? string.Empty :
-                header.Equals("VAR_INPUT") ? "Input" :
-                header.Equals("VAR_OUTPUT") ? "Output" :
-                header.Equals("VAR") ? "Local" : string.Empty;
+            var (name, type) = ParseVariable(variableText);
+            Name = name;
+            Type = type;
+            Category = category;
         }
 
-        private string[] GetVariableDefinitions(string variableGroupText)
-        {
-            return variableGroupText.Split(',').Where(s => !s.StartsWith("VAR")).ToArray();
-        }
+        public string Name;
+        public string Type;
+        public FbkVarCategory Category;
 
-        private (string name, string type) ParseVariable(string variableText)
+        public static (string name, string type) ParseVariable(string variableText)
         {
             var match = Regex.Match(variableText, @"^(\w+).*\W(\w+);$");
             return match.Success ? (match.Groups[1].Value, match.Groups[2].Value) : (string.Empty, string.Empty);
         }
 
+        public static FbkVarCategory GetVariableCategory(string variableGroupText)
+        {
+            var header = variableGroupText.Split(',').FirstOrDefault();
+
+            return string.IsNullOrWhiteSpace(header) ? FbkVarCategory.Other :
+                header.Equals("VAR_INPUT") ? FbkVarCategory.Input :
+                header.Equals("VAR_OUTPUT") ? FbkVarCategory.Output :
+                header.Equals("VAR") ? FbkVarCategory.Local : FbkVarCategory.Other;
+        }
+
+        public static string[] GetVariableDefinitions(string variableGroupText)
+        {
+            return variableGroupText.Split(',').Where(s => !s.StartsWith("VAR")).ToArray();
+        }
+    }
+
+    public enum FbkVarCategory
+    {
+        Input,
+        Output,
+        Local,
+        Other
     }
 }
